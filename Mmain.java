42a35d6917164096184929f440943302ba04aa03
Naima Bedrane <naimabedrane@Naimas-MacBook-Pro.local>
2024-01-01 20:41:28 -0600

    first commit

---
+++ Mmain.java
@@ -0,0 +1,9 @@
+package Main;
+
+public class Mmain {
+    public static void main(String[] args) {
+        System.out.println("Hello Naima");
+        System.out.println("Hello Hello");
+        System.out.println("Fatna 3issa");
+    }
+}
